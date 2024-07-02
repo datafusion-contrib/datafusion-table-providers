64e812fe117532076f5bb1a3a57075d96409a11d
Phillip LeBlanc <phillip@leblanc.tech>
2024-07-02 16:31:16 +0900

    Add Postgres table provider

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
+pub mod delete;
 pub mod sql;
 pub mod util;
+
+#[cfg(feature = "postgres")]
+pub mod postgres;
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,1 +1,47 @@
+use datafusion::logical_expr::Expr;
+use snafu::prelude::*;
+use std::collections::HashMap;
+use std::hash::Hash;
+
+use crate::sql::sql_provider_datafusion::expr::{self, Engine};
+
+pub mod column_reference;
+pub mod constraints;
+pub mod indexes;
 pub mod ns_lookup;
+pub mod on_conflict;
+pub mod secrets;
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("Unable to generate SQL: {source}"))]
+    UnableToGenerateSQL { source: expr::Error },
+}
+
+pub fn filters_to_sql(filters: &[Expr], engine: Option<Engine>) -> Result<String, Error> {
+    Ok(filters
+        .iter()
+        .map(|expr| expr::to_sql_with_engine(expr, engine))
+        .collect::<expr::Result<Vec<_>>>()
+        .context(UnableToGenerateSQLSnafu)?
+        .join(" AND "))
+}
+
+#[must_use]
+pub fn hashmap_from_option_string<K, V>(hashmap_option_str: &str) -> HashMap<K, V>
+where
+    K: for<'a> From<&'a str> + Eq + Hash,
+    V: for<'a> From<&'a str> + Default,
+{
+    hashmap_option_str
+        .split(';')
+        .map(|index| {
+            let parts: Vec<&str> = index.split(':').collect();
+            if parts.len() == 2 {
+                (K::from(parts[0]), V::from(parts[1]))
+            } else {
+                (K::from(index), V::default())
+            }
+        })
+        .collect()
+}
