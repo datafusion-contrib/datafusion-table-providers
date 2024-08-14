bf8bc905f45159ad605ddba9d66ee016dd3f3e84
peasee <98815791+peasee@users.noreply.github.com>
2024-08-14 17:15:28 +1000

    feat: Add support for ATTACHing SQLite databases together

--- src/sql/db_connection_pool/sqlitepool.rs
+++ src/sql/db_connection_pool/sqlitepool.rs
@@ -1,4 +1,7 @@
+use std::sync::Arc;
+
 use async_trait::async_trait;
+use sha2::{Digest, Sha256};
 use snafu::{prelude::*, ResultExt};
 use tokio_rusqlite::{Connection, ToSql};
 
@@ -17,10 +20,72 @@ pub enum Error {
     NoPathError {},
 }
 
+pub struct SqliteConnectionPoolFactory {
+    path: Arc<str>,
+    mode: Mode,
+    attach_databases: Option<Vec<Arc<str>>>,
+}
+
+fn hash_string(val: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(val);
+    hasher.finalize().iter().fold(String::new(), |mut hash, b| {
+        hash.push_str(&format!("{b:02x}"));
+        hash
+    })
+}
+
+impl SqliteConnectionPoolFactory {
+    pub fn new(path: &str, mode: Mode) -> Self {
+        SqliteConnectionPoolFactory {
+            path: path.into(),
+            mode,
+            attach_databases: None,
+        }
+    }
+
+    #[must_use]
+    pub fn with_databases(mut self, attach_databases: Option<Vec<Arc<str>>>) -> Self {
+        self.attach_databases = attach_databases;
+        self
+    }
+
+    pub async fn build(self) -> Result<SqliteConnectionPool> {
+        let join_push_down = match (self.mode, &self.attach_databases) {
+            (Mode::File, Some(attach_databases)) => {
+                let mut attach_databases = attach_databases.clone();
+
+                if !attach_databases.contains(&self.path) {
+                    attach_databases.push(Arc::clone(&self.path));
+                }
+
+                attach_databases.sort();
+
+                let attach_hash = hash_string(&attach_databases.join(";"));
+                JoinPushDown::AllowedFor(attach_hash) // push down is allowed cross-database when they're attached together
+                                                      // hash the list of databases to generate the comparison for push down
+            }
+            (Mode::File, None) => JoinPushDown::AllowedFor(self.path.to_string()),
+            _ => JoinPushDown::Disallow,
+        };
+
+        let attach_databases = self.attach_databases.unwrap_or_default();
+        let pool =
+            SqliteConnectionPool::new(&self.path, self.mode, join_push_down, attach_databases)
+                .await?;
+
+        pool.setup().await?;
+
+        Ok(pool)
+    }
+}
+
 pub struct SqliteConnectionPool {
     conn: Connection,
     join_push_down: JoinPushDown,
     mode: Mode,
+    path: Arc<str>,
+    attach_databases: Vec<Arc<str>>,
 }
 
 impl SqliteConnectionPool {
@@ -33,40 +98,32 @@ impl SqliteConnectionPool {
     ///
     /// Returns an error if there is a problem creating the connection pool.
     #[allow(clippy::needless_pass_by_value)]
-    pub async fn new(path: &str, mode: Mode) -> Result<Self> {
-        let (conn, join_push_down) = match mode {
-            Mode::Memory => (
-                Connection::open_in_memory()
-                    .await
-                    .context(ConnectionPoolSnafu)?,
-                JoinPushDown::Disallow,
-            ),
-            Mode::File => (
-                Connection::open(path.to_string())
-                    .await
-                    .context(ConnectionPoolSnafu)?,
-                JoinPushDown::AllowedFor(path.to_string()),
-            ),
+    pub async fn new(
+        path: &str,
+        mode: Mode,
+        join_push_down: JoinPushDown,
+        attach_databases: Vec<Arc<str>>,
+    ) -> Result<Self> {
+        let conn = match mode {
+            Mode::Memory => Connection::open_in_memory()
+                .await
+                .context(ConnectionPoolSnafu)?,
+
+            Mode::File => Connection::open(path.to_string())
+                .await
+                .context(ConnectionPoolSnafu)?,
         };
 
         Ok(SqliteConnectionPool {
             conn,
             join_push_down,
             mode,
+            attach_databases,
+            path: path.into(),
         })
     }
 
-    #[must_use]
-    pub fn connect_sync(&self) -> Box<dyn DbConnection<Connection, &'static (dyn ToSql + Sync)>> {
-        Box::new(SqliteConnection::new(self.conn.clone()))
-    }
-}
-
-#[async_trait]
-impl DbConnectionPool<Connection, &'static (dyn ToSql + Sync)> for SqliteConnectionPool {
-    async fn connect(
-        &self,
-    ) -> Result<Box<dyn DbConnection<Connection, &'static (dyn ToSql + Sync)>>> {
+    pub async fn setup(&self) -> Result<()> {
         let conn = self.conn.clone();
 
         // these configuration options are only applicable for file-mode databases
@@ -85,7 +142,43 @@ impl DbConnectionPool<Connection, &'static (dyn ToSql + Sync)> for SqliteConnect
             })
             .await
             .context(ConnectionPoolSnafu)?;
+
+            // database attachments are only supported for file-mode databases
+            let attach_databases = self
+                .attach_databases
+                .iter()
+                .enumerate()
+                .map(|(i, db)| format!("ATTACH DATABASE '{db}' AS accelerator_{i}"));
+
+            for attachment in attach_databases {
+                if attachment == *self.path {
+                    continue;
+                }
+
+                conn.call(move |conn| {
+                    conn.execute(&attachment, [])?;
+                    Ok(())
+                })
+                .await
+                .context(ConnectionPoolSnafu)?;
+            }
         }
+
+        Ok(())
+    }
+
+    #[must_use]
+    pub fn connect_sync(&self) -> Box<dyn DbConnection<Connection, &'static (dyn ToSql + Sync)>> {
+        Box::new(SqliteConnection::new(self.conn.clone()))
+    }
+}
+
+#[async_trait]
+impl DbConnectionPool<Connection, &'static (dyn ToSql + Sync)> for SqliteConnectionPool {
+    async fn connect(
+        &self,
+    ) -> Result<Box<dyn DbConnection<Connection, &'static (dyn ToSql + Sync)>>> {
+        let conn = self.conn.clone();
 
         Ok(Box::new(SqliteConnection::new(conn)))
     }
