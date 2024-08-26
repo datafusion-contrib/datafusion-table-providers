e3f58af2e35311a46de39b64692b280c0fd450b2
peasee <98815791+peasee@users.noreply.github.com>
2024-08-26 13:24:45 +1000

    fix: CHECKPOINT DuckDB after finishing writing

--- src/duckdb.rs
+++ src/duckdb.rs
@@ -85,6 +85,11 @@ pub enum Error {
     #[snafu(display("Unable to commit transaction: {source}"))]
     UnableToCommitTransaction { source: duckdb::Error },
 
+    #[snafu(display("Unable to checkpoint duckdb: {source}"))]
+    UnableToCheckpoint {
+        source: Box<dyn std::error::Error + Send + Sync>,
+    },
+
     #[snafu(display("Unable to begin duckdb transaction: {source}"))]
     UnableToBeginTransaction { source: duckdb::Error },
 
