d1fd8abf9e3936da6cb7af4f0a210095eee44ff1
Tim Saucer <timothy.saucer@maymobility.com>
2024-12-09 19:54:26 -0500

    Initial commit of sqlite table provider in python example

---
+++ python/python/datafusion_table_providers/sqlite.py
@@ -0,0 +1,19 @@
+
+from typing import Any, List, Optional
+from . import _internal
+
+class SqliteTableFactory:
+
+    def __init__(self, path: str, mode: str, busy_timeout_s: float, attach_databases: Optional[List[str]] = None) -> None:
+        """Create a new :py:class:`SessionConfig` with the given configuration options.
+
+        Args:
+            config_options: Configuration options.
+        """
+        self._raw = _internal.sqlite.RawSqliteTableFactory(path, mode, busy_timeout_s, attach_databases)
+    
+    def tables(self) -> List[str]:
+        return self._raw.tables()
+    
+    def get_table(self, table_reference: str) -> Any:
+        return self._raw.get_table(table_reference)
---
+++ python/src/lib.rs
@@ -0,0 +1,45 @@
+use std::{ffi::CString, sync::Arc};
+
+use datafusion::catalog::TableProvider;
+use datafusion_ffi::table_provider::FFI_TableProvider;
+use pyo3::{prelude::*, types::PyCapsule};
+
+#[pyclass(module = "datafusion_table_providers._internal")]
+struct RawTableProvider {
+    pub(crate) table: Arc<dyn TableProvider + Send>,
+    pub(crate) supports_pushdown_filters: bool,
+}
+
+#[pymethods]
+impl RawTableProvider {
+    fn __datafusion_table_provider__<'py>(
+        &self,
+        py: Python<'py>,
+    ) -> PyResult<Bound<'py, PyCapsule>> {
+        let name = CString::new("datafusion_table_provider").unwrap();
+
+        let provider = FFI_TableProvider::new(Arc::clone(&self.table), self.supports_pushdown_filters);
+
+        PyCapsule::new_bound(py, provider, Some(name.clone()))
+    }
+}
+
+pub mod duckdb;
+pub mod flight;
+pub mod mysql;
+pub mod odbc;
+pub mod postgres;
+pub mod sqlite;
+pub mod utils;
+
+#[pymodule]
+// module name need to match project name
+fn _internal(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<RawTableProvider>()?;
+
+    let sqlite = PyModule::new_bound(py, "sqlite")?;
+    sqlite::init_module(&sqlite)?;
+    m.add_submodule(&sqlite)?;
+
+    Ok(())
+}
