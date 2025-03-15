3cf10263463d8edc207214ac74dc00be25b3f3a8
Gaurav Verma <princeverma@gmail.com>
2025-03-16 02:57:58 +0530

    comment timeone setting

--- src/sql/db_connection_pool/mysqlpool.rs
+++ src/sql/db_connection_pool/mysqlpool.rs
@@ -256,10 +256,10 @@ impl DbConnectionPool<mysql_async::Conn, &'static (dyn ToValue + Sync)> for MySQ
         let mut conn = pool.get_conn().await.context(MySQLConnectionSnafu)?;
 
         // Set MySQL session default time zone to UTC to match Datafusion
-        let _: Vec<Row> = conn
-            .exec("SET time_zone = '+00:00'", Params::Empty)
-            .await
-            .context(MySQLConnectionSnafu)?;
+        // let _: Vec<Row> = conn
+        //     .exec("SET time_zone = '+00:00'", Params::Empty)
+        //     .await
+        //     .context(MySQLConnectionSnafu)?;
 
         Ok(Box::new(MySQLConnection::new(conn)))
     }
