43eeeaf35d2deaaf4d24c82f2989799acfeca466
Crystal Zhou <crystal.zhouxiaoyue@hotmail.com>
2025-03-23 13:10:42 -0400

    Revert a false change

--- core/src/flight.rs
+++ core/src/flight.rs
@@ -5,9 +5,9 @@
 // to you under the Apache License, Version 2.0 (the
 // "License"); you may not use this file except in compliance
 // with the License.  You may obtain a copy of the License at
-
+//
 //   http://www.apache.org/licenses/LICENSE-2.0
-
+//
 // Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -15,8 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// ! Generic [FlightTableFactory] that can connect to Arrow Flight services,
-// ! with a [sql::FlightSqlDriver] provided out-of-the-box.
+//! Generic [FlightTableFactory] that can connect to Arrow Flight services,
+//! with a [sql::FlightSqlDriver] provided out-of-the-box.
 
 use std::any::Any;
 use std::collections::HashMap;
