733a7c2dd5699408213e18faa806291a520af997
Crystal Zhou <45134936+crystalxyz@users.noreply.github.com>
2025-04-09 16:02:27 -0400

    Update core/src/sql/db_connection_pool/runtime.rs
    Co-authored-by: Phillip LeBlanc <phillip@leblanc.tech>

--- core/src/sql/db_connection_pool/runtime.rs
+++ core/src/sql/db_connection_pool/runtime.rs
@@ -6,8 +6,6 @@ use std::{future::Future, sync::OnceLock};
 use tokio::runtime::{Handle, Runtime};
 
 pub fn get_tokio_runtime() -> &'static Runtime {
-    // TODO: this function is a repetition of python/src/utils.rs::get_tokio_runtime.
-    // Think about how to refactor it
     static RUNTIME: OnceLock<Runtime> = OnceLock::new();
     RUNTIME.get_or_init(|| Runtime::new().expect("Failed to create Tokio runtime"))
 }
