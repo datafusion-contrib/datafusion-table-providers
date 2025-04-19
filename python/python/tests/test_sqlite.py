97a67b8f0a4601d3a762812d4eee57c832c77650
Crystal Zhou <crystal.zhouxiaoyue@hotmail.com>
2025-04-19 17:29:48 +0800

    fix some bugs in workflow

--- python/python/tests/test_sqlite.py
+++ python/python/tests/test_sqlite.py
@@ -1,23 +1,22 @@
 import unittest
 import os
 from datafusion import SessionContext
-from datafusion_table_providers import duckdb
+from datafusion_table_providers import sqlite
 
-class TestDuckDBIntegration(unittest.TestCase):
+class TestsqliteIntegration(unittest.TestCase):
     def setUp(self):
         """Set up the test environment"""
         self.ctx = SessionContext()
-        self.db_path = os.path.join(os.path.dirname(__file__), "..", "..", "..", "core", "examples", "duckdb_example.db")
-        self.pool_readonly = duckdb.DuckDBTableFactory(self.db_path, duckdb.AccessMode.ReadOnly)
-        self.pool_readwrite = duckdb.DuckDBTableFactory(self.db_path)
+        self.db_path = os.path.join(os.path.dirname(__file__), "..", "..", "..", "core", "examples", "sqlite_example.db")
+        self.pool = sqlite.SqliteTableFactory(self.db_path, "file", 3.0, None)
 
     def test_get_tables(self):
         """Test retrieving tables from the database"""
-        tables = self.pool_readonly.tables()
+        tables = self.pool.tables()
         self.assertIsInstance(tables, list)
         self.assertTrue(len(tables) == 2)
         self.assertEqual(tables, ["companies", "projects"])
-
+        
     def test_query_companies(self):
         """Test querying companies table with SQL"""
         self.ctx.register_table_provider("companies", self.pool.get_table("companies"))
@@ -29,7 +28,7 @@ class TestDuckDBIntegration(unittest.TestCase):
         # Verify single row returned with name = Microsoft
         self.assertEqual(len(result), 1)
         self.assertEqual(str(result[0]['name'][0]), "Microsoft")
-
+        
     def test_complex_query(self):
         """Test querying companies table with SQL"""
         self.ctx.register_table_provider("companies", self.pool.get_table("companies"))
@@ -42,30 +41,20 @@ class TestDuckDBIntegration(unittest.TestCase):
             WHERE companies.id = projects.id"""
         )
         result = df.collect()
-
+    
         self.assertEqual(len(result), 1)
         self.assertEqual(str(result[0]['company_name'][0]), "Microsoft")
         self.assertEqual(str(result[0]['project_name'][0]), "DataFusion")
-
-    def test_write_fails_readonly(self):
+        
+    def test_write_fails(self):
         """Test that writing fails when database is opened read-only"""
         table_name = "companies"
-        self.ctx.register_table_provider(table_name, self.pool_readonly.get_table("companies"))
+        self.ctx.register_table_provider(table_name, self.pool.get_table("companies"))
         
         with self.assertRaises(Exception):
             tmp = self.ctx.sql("INSERT INTO companies VALUES (3, 'Test Corp', 'TEST')")
             tmp.collect() # this will trigger the execution of the query
 
-    def test_write_fails_readwrite(self):
-        """Test that writing fails when database is opened read-only"""
-        # Insertion fails because duckdb does not implement write operations even when
-        # database is opened in read-write mode.
-        table_name = "companies"
-        self.ctx.register_table_provider(table_name, self.pool_readwrite.get_table("companies"))
-        
-        with self.assertRaises(Exception):
-            tmp = self.ctx.sql("INSERT INTO companies VALUES (3, 'Test Corp', 'TEST')")
-            tmp.collect()
 
 if __name__ == '__main__':
     unittest.main() 
