47208d992a535f98fffca153a00cebccc8646ac4
Phillip LeBlanc <phillip@leblanc.tech>
2025-06-05 16:53:27 +0900

    Don't pushdown filters with subqueries (#316)
    * Don't pushdown filters with subqueries.
    
    * Use simpler TreeNode recursion

---
+++ core/src/sql/sql_provider_datafusion/expr.rs
@@ -0,0 +1,68 @@
+use datafusion::{
+    common::tree_node::{TreeNode, TreeNodeRecursion},
+    error::Result,
+    logical_expr::Expr,
+};
+
+pub(super) fn expr_contains_subquery(expr: &Expr) -> Result<bool> {
+    let mut contains_subquery = false;
+    expr.apply(|expr| match expr {
+        Expr::ScalarSubquery(_) | Expr::InSubquery(_) | Expr::Exists(_) => {
+            contains_subquery = true;
+            Ok(TreeNodeRecursion::Stop)
+        }
+        _ => Ok(TreeNodeRecursion::Continue),
+    })?;
+    Ok(contains_subquery)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use datafusion::common::ScalarValue;
+    use datafusion::logical_expr::{
+        col, expr::InList, BinaryExpr, Expr, LogicalPlanBuilder, Operator,
+    };
+    use datafusion::prelude::scalar_subquery;
+
+    #[test]
+    fn test_literal_no_subquery() {
+        let expr = Expr::Literal(ScalarValue::Int32(Some(1)));
+        assert!(!expr_contains_subquery(&expr).expect("literal should not contain subquery"));
+    }
+
+    #[test]
+    fn test_scalar_subquery() {
+        let plan = LogicalPlanBuilder::empty(false).build().unwrap();
+        let subquery = scalar_subquery(plan.into());
+        assert!(expr_contains_subquery(&subquery).expect("subquery should contain subquery"));
+    }
+
+    #[test]
+    fn test_binary_expr_with_subquery() {
+        let plan = LogicalPlanBuilder::empty(false).build().unwrap();
+        let left = scalar_subquery(plan.into());
+        let right = Expr::Literal(ScalarValue::Int32(Some(2)));
+        let expr = Expr::BinaryExpr(BinaryExpr {
+            left: Box::new(left),
+            op: Operator::Eq,
+            right: Box::new(right),
+        });
+        assert!(expr_contains_subquery(&expr).expect("binary expr should contain subquery"));
+    }
+
+    #[test]
+    fn test_inlist_with_subquery() {
+        let plan = LogicalPlanBuilder::empty(false).build().unwrap();
+        let list = vec![
+            Expr::Literal(ScalarValue::Int32(Some(1))),
+            scalar_subquery(plan.into()),
+        ];
+        let expr = Expr::InList(InList {
+            expr: Box::new(col("a")),
+            list,
+            negated: false,
+        });
+        assert!(expr_contains_subquery(&expr).expect("inlist should contain subquery"));
+    }
+}
