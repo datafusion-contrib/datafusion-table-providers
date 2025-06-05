c8a325fbfc35e26c0cfdece373b6d03459618060
Phillip LeBlanc <phillip@leblanc.tech>
2025-06-05 14:50:14 +0900

    Use simpler TreeNode recursion

--- core/src/sql/sql_provider_datafusion/expr.rs
+++ core/src/sql/sql_provider_datafusion/expr.rs
@@ -1,140 +1,19 @@
 use datafusion::{
+    common::tree_node::{TreeNode, TreeNodeRecursion},
     error::Result,
-    logical_expr::{
-        expr::{Alias, InList, WindowFunction},
-        BinaryExpr, Case, Cast, Expr, GroupingSet, TryCast,
-    },
+    logical_expr::Expr,
 };
 
 pub(super) fn expr_contains_subquery(expr: &Expr) -> Result<bool> {
-    match expr {
-        Expr::InList(InList { expr, list, .. }) => {
-            let list_expr = list
-                .iter()
-                .map(expr_contains_subquery)
-                .collect::<Result<Vec<_>>>()?;
-            Ok(list_expr.iter().any(|e| *e) || expr_contains_subquery(expr)?)
+    let mut contains_subquery = false;
+    expr.apply(|expr| match expr {
+        Expr::ScalarSubquery(_) | Expr::InSubquery(_) | Expr::Exists(_) => {
+            contains_subquery = true;
+            Ok(TreeNodeRecursion::Stop)
         }
-        Expr::ScalarSubquery(_) | Expr::InSubquery(_) | Expr::Exists(_) => Ok(true),
-        Expr::BinaryExpr(BinaryExpr { left, right, .. }) => {
-            Ok(expr_contains_subquery(left)? || expr_contains_subquery(right)?)
-        }
-        Expr::Case(Case {
-            expr,
-            when_then_expr,
-            else_expr,
-        }) => {
-            if let Some(e) = expr {
-                if expr_contains_subquery(e)? {
-                    return Ok(true);
-                }
-            }
-            for (when, then) in when_then_expr {
-                if expr_contains_subquery(when)? || expr_contains_subquery(then)? {
-                    return Ok(true);
-                }
-            }
-            if let Some(e) = else_expr {
-                if expr_contains_subquery(e)? {
-                    return Ok(true);
-                }
-            }
-            Ok(false)
-        }
-        Expr::Cast(Cast { expr, .. }) | Expr::TryCast(TryCast { expr, .. }) => {
-            expr_contains_subquery(expr)
-        }
-        Expr::Alias(Alias { expr, .. }) => expr_contains_subquery(expr),
-        Expr::WindowFunction(WindowFunction {
-            args,
-            partition_by,
-            order_by,
-            ..
-        }) => {
-            for arg in args {
-                if expr_contains_subquery(arg)? {
-                    return Ok(true);
-                }
-            }
-            for part in partition_by {
-                if expr_contains_subquery(part)? {
-                    return Ok(true);
-                }
-            }
-            for ord in order_by {
-                if expr_contains_subquery(&ord.expr)? {
-                    return Ok(true);
-                }
-            }
-            Ok(false)
-        }
-        Expr::AggregateFunction(agg) => {
-            for arg in &agg.args {
-                if expr_contains_subquery(arg)? {
-                    return Ok(true);
-                }
-            }
-            if let Some(filter) = &agg.filter {
-                if expr_contains_subquery(filter)? {
-                    return Ok(true);
-                }
-            }
-            Ok(false)
-        }
-        Expr::Not(e)
-        | Expr::Negative(e)
-        | Expr::IsNull(e)
-        | Expr::IsNotNull(e)
-        | Expr::IsTrue(e)
-        | Expr::IsNotTrue(e)
-        | Expr::IsFalse(e)
-        | Expr::IsNotFalse(e)
-        | Expr::IsUnknown(e)
-        | Expr::IsNotUnknown(e) => expr_contains_subquery(e),
-        Expr::SimilarTo(like) | Expr::Like(like) => {
-            if expr_contains_subquery(&like.expr)? || expr_contains_subquery(&like.pattern)? {
-                return Ok(true);
-            }
-            Ok(false)
-        }
-        Expr::GroupingSet(grouping_set) => match grouping_set {
-            GroupingSet::GroupingSets(sets) => {
-                for set in sets {
-                    for e in set {
-                        if expr_contains_subquery(e)? {
-                            return Ok(true);
-                        }
-                    }
-                }
-                Ok(false)
-            }
-            GroupingSet::Cube(cube) | GroupingSet::Rollup(cube) => {
-                for e in cube {
-                    if expr_contains_subquery(e)? {
-                        return Ok(true);
-                    }
-                }
-                Ok(false)
-            }
-        },
-        Expr::Placeholder(_)
-        | Expr::ScalarFunction(_)
-        | Expr::Column(_)
-        | Expr::Literal(_)
-        | Expr::Wildcard { .. }
-        | Expr::ScalarVariable(_, _) => Ok(false),
-        Expr::Between(between) => {
-            if expr_contains_subquery(&between.expr)?
-                || expr_contains_subquery(&between.low)?
-                || expr_contains_subquery(&between.high)?
-            {
-                return Ok(true);
-            }
-            Ok(false)
-        }
-        Expr::OuterReferenceColumn(_, _) => Ok(false),
-        Expr::Unnest(_) => Ok(false),
-    }
+        _ => Ok(TreeNodeRecursion::Continue),
+    })?;
+    Ok(contains_subquery)
 }
 
 #[cfg(test)]
