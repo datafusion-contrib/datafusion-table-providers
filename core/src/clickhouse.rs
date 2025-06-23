5c6ce77d46e906bfe03119dc88b03c73bc264c0b
Satyam Singh <trueleo@protonmail.com>
2025-06-23 10:33:57 +0530

    Remove unused comment

--- core/src/clickhouse.rs
+++ core/src/clickhouse.rs
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// use async_trait::async_trait;
 use clickhouse::Client;
 use datafusion::arrow::datatypes::SchemaRef;
 use datafusion::catalog::TableProvider;
