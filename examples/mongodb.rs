09e0a59f1c5c801e63b2ce6ad129cfd6e04a4e43
Viktor Yershov <krinart@gmail.com>
2025-07-12 17:29:09 -0700

    Fix mongo example

--- examples/mongodb.rs
+++ examples/mongodb.rs
@@ -41,7 +41,7 @@ async fn main(){
     let mongodb_params = to_secret_map(HashMap::from([
         (
             "connection_string".to_string(),
-            "mongodb://root:password@localhost:27017/mongo_db?authSource=admin&tls=true".to_string(),
+            "mongodb://root:password@localhost:27017/mongo_db?authSource=admin&tls=false".to_string(),
         ),
     ]));
 
