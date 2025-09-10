477cb9baf424477b437956590efa0ae1eff63b53
jeadie <jack@spice.ai>
2025-09-09 17:15:28 -0700

    fixes

--- src/sql/sql_provider_datafusion/federation.rs
+++ src/sql/sql_provider_datafusion/federation.rs
@@ -1,6 +1,8 @@
 use crate::sql::db_connection_pool::{dbconnection::get_schema, JoinPushDown};
 use async_trait::async_trait;
-use datafusion_federation::sql::{SQLExecutor, SQLFederationProvider, SQLTableSource};
+use datafusion_federation::sql::{
+    RemoteTableRef, SQLExecutor, SQLFederationProvider, SQLTableSource,
+};
 use datafusion_federation::{FederatedTableProviderAdaptor, FederatedTableSource};
 use futures::TryStreamExt;
 use snafu::prelude::*;
@@ -26,9 +28,10 @@ impl<T, P> SqlTable<T, P> {
         let table_name = self.table_reference.clone();
         let schema = Arc::clone(&self.schema);
         let fed_provider = Arc::new(SQLFederationProvider::new(self));
+
         Ok(Arc::new(SQLTableSource::new_with_schema(
             fed_provider,
-            table_name,
+            RemoteTableRef::from(table_name),
             schema,
         )))
     }
