fe0b78ff01a329448e3d533192967ba05a88cdd1
Sergei Grebnov <sergei.grebnov@gmail.com>
2026-03-27 14:10:02 +0300

    fix(PostgreSQL): fix schema discovery for PostgreSQL partitioned tables (#587)

--- core/src/sql/db_connection_pool/dbconnection/postgresconn.rs
+++ core/src/sql/db_connection_pool/dbconnection/postgresconn.rs
@@ -105,7 +105,7 @@ LEFT JOIN pg_type t ON t.oid = a.atttypid
 LEFT JOIN custom_type_details custom ON custom.typname = t.typname
 WHERE ns.nspname = $1
     AND cls.relname = $2
-    AND cls.relkind IN ('r','v','m')  -- covers tables, normal views, & materialized views
+    AND cls.relkind IN ('r','v','m','p')  -- covers tables, normal views, materialized views, & partitioned tables
     AND a.attnum > 0
     AND NOT a.attisdropped
 ORDER BY a.attnum;
@@ -132,7 +132,7 @@ LEFT JOIN pg_type t ON t.oid = a.atttypid
 LEFT JOIN pg_type et ON t.typelem = et.oid
 WHERE ns.nspname = $1
     AND cls.relname = $2
-    AND cls.relkind IN ('r','v','m')
+    AND cls.relkind IN ('r','v','m','p')  -- 'p' unused by Redshift today, included for parity with standard PG query and future support
     AND a.attnum > 0
     AND NOT a.attisdropped
 ORDER BY a.attnum;
